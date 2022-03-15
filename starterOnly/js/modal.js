a132fba848aefd61724a030ead4cd99f239577f1
François <itoutouberny@gmail.com>
2022-03-15 19:34:22 +0100

    feat(modal): add modal close feature
    Took 16 minutes
    
    Took 18 minutes

--- starterOnly/js/modal.js
+++ starterOnly/js/modal.js
@@ -1,19 +1,42 @@
-function editNav() {
-  const navigation = document.querySelector(".main-header__nav");
+/*
+  Index
+  ---------- ---------- ---------- ---------- ----------
+  • Mobile navigation handler
+*/
+
+/*
+  • Mobile navigation handler
+  ---------- ---------- ---------- ---------- ----------
+*/
+const menuToggle = document.querySelector('.menu-toggle');
+const navigation = document.querySelector('.main-header__nav');
+
+// click toggle event
+menuToggle.addEventListener('click', () => {
   navigation.classList.toggle('--on-mobile')
-}
+})
 
-// DOM Elements
-const modalbg = document.querySelector(".bground");
+
+/*
+  • Modal management handler
+  ---------- ---------- ---------- ---------- ----------
+*/
+
+const modal = document.querySelector(".modal");
 const modalBtn = document.querySelectorAll(".modal-btn");
+const modalClose = document.querySelector(".modal .close");
+
+// open modal event
+modalBtn.forEach((btn) => btn.addEventListener("click", () => {
+  modal.classList.add('--is-open')
+}));
+
+// close modal event
+
+modalClose.addEventListener('click', () => {
+  modal.classList.remove('--is-open')
+})
+
+
 const formData = document.querySelectorAll(".formData");
-
-// launch modal event
-modalBtn.forEach((btn) => btn.addEventListener("click", launchModal));
-
-// launch modal form
-function launchModal() {
-  modalbg.style.display = "block";
-}
-
 

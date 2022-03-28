ec1670f8a89a06e93a215b98f9c58f8c8d31f302
Benry <itoutouberny@gmail.com>
2022-03-28 15:42:23 +0200

    feat(modal-form): add data validation and submit button disable depend on form validity
    Took 16 minutes
    
    Took 2 hours 48 minutes

--- starterOnly/js/modal.js
+++ starterOnly/js/modal.js
@@ -12,17 +12,56 @@
 */
 
 const INPUT_REGEX = {
-  name: /^[ a-zA-Z-ç\-\']{2,}$/,
-  email: /^[a-zA-Z0-9.!#$%&'*+/=?^_`{|}~-]+@[a-zA-Z0-9](?:[a-zA-Z0-9-]{0,61}[a-zA-Z0-9])?(?:\.[a-zA-Z0-9](?:[a-zA-Z0-9-]{0,61}[a-zA-Z0-9])?)*$/,
+  name: /^[ a-zA-Z-ç\-']{2,}$/,
+  email: /^[a-zA-Z0-9.!#$%&'*+/=?^_`{|}~-]+@[a-zA-Z0-9](?:[a-zA-Z0-9-]{0,61}[a-zA-Z0-9])?(?:\.[a-zA-Z0](?:[a-zA-Z0]{0,61}[a-zA-Z0])?)*$/,
   date: /^\d{4}-(0[1-9]|1[0-2])-(0[1-9]|[12][0-9]|3[01])$/,
   number: /^\d+$/,
 }
 
-function checkValueValidity(input, regexId) {
-  const inputValue = input.value.trim();
+function checkFormValidity() {
+  const formData = document.querySelectorAll(".formData");
+  let formValid = true;
+
+  formData.forEach((item) => {
+    if (item.getAttribute("data-error-visible") === "true") {
+      formValid = false;
+    }
+
+    if (item.getAttribute("data-validity") === "false") {
+      formValid = false;
+    }
+  });
+
+  return formValid;
+}
+
+function updateSubmitButton() {
+  const submitButton = modalForm.querySelector(".btn-submit");
+  let formValid = checkFormValidity();
+
+  if (formValid) {
+    submitButton.removeAttribute("disabled");
+  } else {
+    submitButton.setAttribute("disabled", "true");
+  }
+}
+
+function checkInputValidity(item) {
+  let testResult;
+  const inputValue = item.value.trim();
+  const regexId = item.parentElement.getAttribute("data-validator");
   const inputRegex = INPUT_REGEX[regexId];
 
-  return inputRegex.test(inputValue);
+  testResult = inputRegex.test(inputValue);
+
+  if(inputValue) {
+    if (testResult) {
+      item.parentElement.setAttribute("data-error-visible", "false");
+    } else {
+      item.parentElement.setAttribute("data-error-visible", "true");
+      return false;
+    }
+  }
 }
 
 /*
@@ -61,29 +100,54 @@ modalClose.addEventListener('click', () => {
   ---------- ---------- ---------- ---------- ----------
 */
 
-
-const formData = document.querySelectorAll(".formData");
-
-formData.forEach(item => {
-  const itemInput = item.querySelector(".text-control");
-
-  if (itemInput) {
-    const regexId = item.getAttribute("data-validator");
-
-    itemInput.addEventListener("change", () => {
-      if (checkValueValidity(itemInput, regexId)) {
-        item.setAttribute("data-error-visible", "false");
-      } else {
-        item.setAttribute("data-error-visible", "true");
-      }
-    });
-  }
-
-});
-
-
 const modalForm = document.querySelector(".modal-form");
 
+if (modalForm) {
+  modalForm.querySelectorAll(".text-control").forEach((item) => {
+    item.addEventListener("change", () => {
+      checkInputValidity(item);
+      updateSubmitButton();
+    });
+  });
+
+  modalForm.querySelectorAll(".required-check").forEach((item) => {
+    item.addEventListener("change", () => {
+      checkSelectValidity(item);
+      updateSubmitButton();
+    });
+  });
+}
+
+function checkSelectValidity(item) {
+  let isValid = false;
+  const checkedOption = item.querySelector("input:checked");
+  console.log(checkedOption);
+
+  if (checkedOption) {
+    isValid = true;
+    item.setAttribute("data-error-visible", "false");
+    item.setAttribute("data-validity", "true");
+  } else {
+    item.setAttribute("data-error-visible", "true");
+    item.setAttribute("data-validity", "false");
+  }
+
+  console.log(isValid);
+  updateSubmitButton();
+  return isValid;
+}
+
 modalForm.addEventListener('submit', event => {
-  event.preventDefault()
+  event.preventDefault();
+
+  modalForm.querySelectorAll(".required-check").forEach((item) => {
+    checkSelectValidity(item);
+  });
+
+  if (checkFormValidity()) {
+    modal.classList.remove('--is-open')
+  }
+
+
+  //check validity of inputs
 })
